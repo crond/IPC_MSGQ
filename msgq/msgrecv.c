43f66fcdd3db5107a673f11584dcd4a8b776a47b
crondglobalname <crondglobalname@cms.com>
2012-10-16 08:32:59 +0530

    msgrecv using type.removed stale files

--- msgq/msgrecv.c
+++ msgq/msgrecv.c
@@ -9,10 +9,18 @@
 #define INFORM          0x01
 #define REPORT          0x02
 #define FATAL           0x03
+typedef struct{
+    int msgType;
+    char msg[256];
+}msgq_t;
+
+
+
+msgq_t mqstructVar;
 key_t keyValue;
 int msgQId;
 char dbgLogStr[256];
-char buffer[512];
+char buffer[260];
 int dbgLog(int dbgLevel,char *log);
 int stopService(char flag);
 
@@ -51,7 +59,8 @@ int main(void)
                         __FILE__,fnName,__LINE__,errno);
         dbgLog(INFORM,dbgLogStr);
         return 0;
-     }       
+     }  
+#if 1     
     errno=0;
      msgQId  =   msgget(keyValue, IPC_CREAT|0644);
     if(msgQId < 0)
@@ -64,19 +73,23 @@ int main(void)
         return 0;
     }     
     snprintf(dbgLogStr,sizeof(dbgLogStr),"%s: MSG Q ID:%d",fnName,msgQId);
+ 
     dbgLog(INFORM,dbgLogStr);
+#endif    
     errno =0;
-    if((msgrcv(msgQId,buffer,sizeof(buffer),0,IPC_NOWAIT)) < 0)
+    if((msgrcv(msgQId,(char *)buffer,sizeof(buffer),2,IPC_NOWAIT)) < 0)
+//    if((msgrcv(msgQId,(char *)&mqstructVar,sizeof(mqstructVar),2,IPC_NOWAIT)) < 0)
     {
-        snprintf(dbgLogStr,sizeof(dbgLogStr),"%s:%s:%d msgrecv error:%d",__FILE__,fnName,__LINE__,errno);
+        snprintf(dbgLogStr,sizeof(dbgLogStr),"%s:%s:%d msgrecv error:%d. %s",__FILE__,fnName,__LINE__,errno,strerror(errno));
         dbgLog(INFORM,dbgLogStr);
     }
     else
     {
+      //  snprintf(dbgLogStr,sizeof(dbgLogStr),"%s Msg Recvd: %s",fnName,mqstructVar.msg);
         snprintf(dbgLogStr,sizeof(dbgLogStr),"%s Msg Recvd: %s",fnName,buffer);
         dbgLog(INFORM,dbgLogStr);
     }
-    stopService(0);
+//    stopService(0);
     return 0;
 }
 
--- msgq/msgsend.c
+++ msgq/msgsend.c
@@ -3,12 +3,25 @@
 #include<sys/ipc.h>
 #include<sys/msg.h>
 #include<errno.h>
+#include<string.h>
+#include<signal.h>
+#include<unistd.h>
+
 
 #define FILE_PATH 	"/home/slacker/audacious/msgq/"
 #define PROJ_ID		    0x55
 #define INFORM          0x01
 #define REPORT          0x02
 #define FATAL           0x03
+
+typedef struct{
+    int msgType;
+    char msg[256];
+}msgq_t;
+
+
+
+msgq_t mqstructVar;
 key_t keyValue;
 int msgQId;
 char dbgLogStr[256];
@@ -38,10 +51,32 @@ int stopService(char flag)
     dbgLog(INFORM,dbgLogStr);
     return 0;
 }
+void *sighandler(int sigNum)
+{
+    static char fnName[]="sighandler():";
+    if( (sigNum == 11)||(sigNum == 2) || (sigNum == 9))
+    {
+        stopService(0);
+        snprintf(dbgLogStr,sizeof(dbgLogStr),"%s: Sig Num:%d",fnName,sigNum);
+        dbgLog(INFORM,dbgLogStr);
+        _exit(0);
+    }
+    else
+      {
+        snprintf(dbgLogStr,sizeof(dbgLogStr),"%s: Sig Num:%d",fnName,sigNum);
+        dbgLog(INFORM,dbgLogStr);
+        _exit(0);
+        
+      } 
+}
 int main(void)
 {
     static char fnName[]="main():";	
-     
+    
+    signal(SIGINT,(void *)&sighandler); 
+    signal(2,(void *)&sighandler); 
+    signal(9,(void *)&sighandler); 
+    signal(11,(void *)&sighandler); 
      errno=0;	
      keyValue = ftok(FILE_PATH,PROJ_ID);
 	 if(keyValue <= 0)
@@ -66,7 +101,9 @@ int main(void)
     snprintf(dbgLogStr,sizeof(dbgLogStr),"%s: MSG Q ID:%d",fnName,msgQId);
     dbgLog(INFORM,dbgLogStr);
     errno =0;
-    if((msgsnd(msgQId,"Hi",3,IPC_NOWAIT)) < 0)
+    mqstructVar.msgType=1;
+    strncpy(mqstructVar.msg,"Hi",3);
+    if((msgsnd(msgQId,(char *)&mqstructVar,sizeof(mqstructVar),IPC_NOWAIT)) < 0)
     {
         snprintf(dbgLogStr,sizeof(dbgLogStr),"%s:%s:%d msgsnd error:%d",__FILE__,fnName,__LINE__,errno);
         dbgLog(INFORM,dbgLogStr);
@@ -75,7 +112,19 @@ int main(void)
     {
         dbgLog(INFORM,"Msg: Hi, sent");
     }
-    sleep(50);
+    errno =0;
+    mqstructVar.msgType=2;
+    strncpy(mqstructVar.msg,"Hello",6);
+    if((msgsnd(msgQId,(char *)&mqstructVar,sizeof(mqstructVar),IPC_NOWAIT)) < 0)
+    {
+        snprintf(dbgLogStr,sizeof(dbgLogStr),"%s:%s:%d msgsnd error:%d",__FILE__,fnName,__LINE__,errno);
+        dbgLog(INFORM,dbgLogStr);
+    }
+    else
+    {
+        dbgLog(INFORM,"Msg: Hello, sent");
+    }
+    sleep(30);
 
     stopService(0);
     return 0;
